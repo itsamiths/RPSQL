08ca02de91c3d81cdb49db028b74edf1ec50e933
itsamiths <mkamithkumar@gmail.com>
2014-12-05 17:47:38 +0530

    test.sh updated with phoenix code

--- test.sh
+++ test.sh
@@ -1,37 +1,36 @@
 #!/bin/sh
 
 ## Poor man's test suite -- if you want to run it,
-## you'll need MySQL on the local machine and
+## you'll need Apache phoenix on the local machine and
 ## adjust the paths/names below as needed
 
-MYSQL=/usr/local/mysql
-DRIVER="$MYSQL/mysql-connector-java.jar"
-DRVCLASS=com.mysql.jdbc.Driver
-
+PSQL=/home/ubuntu/phoenix-4.2.1-bin
+DRIVER="$PSQL/phoenix-4.2.1-client.jar"
+DRVCLASS=org.apache.phoenix.jdbc.PhoenixDriver
 
 ##---- cut here ----
 
 if [ ! -e "$DRIVER" ]; then
-    echo "Cannot find JDBC driver for MySQL, cannot perform MySQL tests"
+    echo "Cannot find JDBC driver for Apache Phoenix, cannot perform PSQL tests"
     exit 1
 fi
 
-echo "library(RJDBC)
+echo "library(RPSQL)
 ## load driver
-MySQL<-JDBC('$DRVCLASS','$DRIVER',"'"`")
+drv<-JDBC('$DRVCLASS','$DRIVER',identifier.quote="'")
 ## connect
-c<-dbConnect(MySQL, "jdbc:mysql://localhost/test")
+c<-dbConnect(drv, "jdbc:phoenix:hbase", "", "")
 ## table operations write/read
-data(iris)
-dbWriteTable(c, "iris", iris, overwrite=TRUE)
-dbReadTable(c, "iris")
+data(mtcars)
+dbWriteTable(c, "rpsql.mtcars", mtcars, overwrite=TRUE)
+dbReadTable(c, "rpsql.mtcars")
 ## simple send query
-fetch(r <- dbSendQuery(c, "SELECT count(*) FROM iris"), 1)
+fetch(r <- dbSendQuery(c, "SELECT count(*) FROM rpsql.mtcars"), 1)
 ## query info - some people need this
 dbGetInfo(r)
 dbHasCompleted(r)
 ## prepared send query
-fetch(dbSendQuery(c, "SELECT Species, count(Species) FROM iris WHERE `Sepal.Width` > ? GROUP BY Species", 3), 1e3)
+fetch(dbSendQuery(c, "SELECT MPG, count(MPG) FROM rpsql.mtcars WHERE CYL  > ? GROUP BY MPG", 3), 1e3)
 ## simple update
 dbSendUpdate(c, "DROP TABLE IF EXISTS foo")
 dbSendUpdate(c, "CREATE TABLE foo (alpha VARCHAR(32), beta INT)")
@@ -40,9 +39,6 @@ dbSendUpdate(c, "INSERT INTO foo VALUES (?, ?)", "foo", 123)
 dbSendUpdate(c, "INSERT INTO foo VALUES (?, ?)", "bar", 456)
 fetch(dbSendQuery(c, "SELECT * FROM foo"), -1)
 ## calls
-## NOTE: there is a bug in MySQL so DROP PROCEDURE IF EXISTS does NOT work!!
-## hence we have to cross our fingers and hope it is not already defined
-## If the call fails, it will leave a dirty db
 dbSendUpdate(c, "CREATE PROCEDURE bar() BEGIN SELECT * FROM foo; END")
 fetch(dbSendQuery(c, "{call bar()}"), -1)
 dbSendUpdate(c, "DROP PROCEDURE bar")
