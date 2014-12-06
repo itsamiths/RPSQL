d8e27f2aaea82477b7564cff92dea3123a51cff6
itsamiths <mkamithkumar@gmail.com>
2014-12-06 21:18:27 +0530

    driver class initial push

---
+++ R/PSQLDriver.R
@@ -0,0 +1,90 @@
+#dbGetInfo
+
+setMethod(
+	"dbGetInfo",
+	"PSQLDriver",
+	def=function(dbObj, ...)
+	list(
+		name="PSQL", 
+		driver.version="0.1-1",
+		DBI.version="0.1-1",
+		client.version=NA,
+		max.connections=NA
+	)
+)
+
+setMethod(
+	"dbGetInfo",
+	"PSQLConnection",
+	def = function(dbObj, ...)
+	list() 
+)
+
+setMethod(
+	"dbGetInfo",
+	"PSQLResult",
+	def=function(dbObj, ...)
+	list(has.completed=TRUE),
+	valueClass="list"
+)
+
+#dbConnect
+
+setMethod(
+	"dbConnect",
+	"PSQLDriver",
+	def=function(drv, url, user='', password='', ...) {
+		jc <- .jcall("java/sql/DriverManager","Ljava/sql/Connection;","getConnection", as.character(url)[1], as.character(user)[1], as.character(password)[1], check=FALSE)
+		if (is.jnull(jc) && !is.jnull(drv@psqlDriver)) {
+			# ok one reason for this to fail is its interaction with rJava's
+			# class loader. In that case we try to load the driver directly.
+			oex <- .jgetEx(TRUE)
+			p <- .jnew("java/util/Properties")
+			if (length(user)==1 && nchar(user))
+				.jcall(p,"Ljava/lang/Object;","setProperty","user",user)
+			if (length(password)==1 && nchar(password))
+				.jcall(p,"Ljava/lang/Object;","setProperty","password",password)
+			l <- list(...)
+			if (length(names(l)))
+				for (n in names(l))
+					.jcall(p, "Ljava/lang/Object;", "setProperty", n, as.character(l[[n]]))
+			jc <- .jcall(drv@psqlDriver, "Ljava/sql/Connection;", "connect", as.character(url)[1], p)
+		}
+		.verify.PSQL.result(jc, "Unable to connect Phoenix JDBC to ",url)
+		new("PSQLConnection", jc=jc, identifier.quote=drv@identifier.quote)
+	},
+	valueClass="PSQLConnection"
+)
+
+#dbUnloadDriver
+
+setMethod(
+	"dbUnloadDriver",
+	"PSQLDriver",
+	def=function(drv, ...) 
+	FALSE
+)
+
+#dbListConnections
+
+setMethod(
+	"dbListConnections",
+	"PSQLDriver",
+	def=function(drv, ...) {
+		warning("Phoenix JDBC driver maintains no list of active connections."); 
+		list() 
+	}
+)
+
+#dbDataType
+
+setMethod(
+	"dbDataType",
+	signature(dbObj="PSQLConnection", obj = "ANY"),
+	def = function(dbObj, obj, ...) {
+		if (is.integer(obj)) "INTEGER"
+		else if (is.numeric(obj)) "DOUBLE"
+		else "VARCHAR"
+	},
+	valueClass = "character"
+)
